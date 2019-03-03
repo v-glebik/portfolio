bded5b5e74b0245d9109fcb484b5c21a10d59ae1
Валерий <maxtercutor@gmail.com>
2019-03-03 12:52:36 +0300

    03.03.19

--- Tasks/strings.js
+++ Tasks/strings.js
@@ -2,9 +2,9 @@
 
 //1) Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
 
-let str = 'str-oka';
-let repeatCount = 3;
-let newStr = str;
+let str = 'stroka',
+    repeatCount = 3,
+    newStr = str;
 
 for(let i = 1; i < repeatCount; i++) newStr = newStr + ', ' + str;
 
@@ -12,6 +12,8 @@ console.log(`Новая Строка: ${newStr}
 Кол-во Символов: ${newStr.length}`);
 
 //2) Дана строка. Вывести первый, последний и средний (если он есть)) символы.
+
+str = 'str-oka';
 
 console.log(`Первый Символ: ${str[0]}
 Последний Символ: ${str[str.length - 1]}`);
@@ -21,6 +23,7 @@ if(str.length % 2 != 0) console.log(`Средний Символ: ${str[(str.len
 //3) Дана строка. Вывести первые три символа и последний три символа, если длина строки больше 5. 
 //Иначе вывести первый символ столько раз, какова длина строки.
 
+str = 'str',
 newStr = '';
 
 if(str.length > 5) {
